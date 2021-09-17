eb58b17ebc8ca0ddac09684368f2a17e6379413c
Yves Hurdequint <hurdequinty@hotmail.com>
2021-09-17 14:19:11 +0800

    picker hidden module on login page

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -20,38 +20,39 @@ const animateNavbar = () => {
     this.classList.add("link-active");
   }
 
-  let initColor = `${window.getComputedStyle(document.documentElement).getPropertyValue('--primary-color')}`
-  initColor = initColor.substring(1);
-  // Init of Pickr
-  const pickr = Pickr.create({
-    el: '.color-picker',
-    theme: 'nano', // or 'monolith', or 'nano'
-    closeWithKey: 'Escape',
-    default: initColor,
-    components: {
+  if (document.querySelector(".color-picker")){
+    let initColor = `${window.getComputedStyle(document.documentElement).getPropertyValue('--primary-color')}`
+    initColor = initColor.substring(1);
+    // Init of Pickr
+    const pickr = Pickr.create({
+      el: '.color-picker',
+      theme: 'nano', // or 'monolith', or 'nano'
+      closeWithKey: 'Escape',
+      default: initColor,
+      components: {
 
-        // Main components
-        hue: true,
+          // Main components
+          hue: true,
 
-        // Input / output Options
-        interaction: {
-            hex: true,
-            input: true,
-            save: true
-        }
+          // Input / output Options
+          interaction: {
+              hex: true,
+              input: true,
+              save: true
+          }
 
-    }
-  });
+      }
+    });
 
-  pickr.on('save', (color, instance) => {
-    // Write in the database
+    pickr.on('save', (color, instance) => {
+      // Write in the database
 
-    pickr.hide();
-  })
-  pickr.on('change', (color, source, instance) => {
-    document.documentElement.style.setProperty("--primary-color", `${color.toHEXA().toString()}`);
-  })
+      pickr.hide();
+    })
+    pickr.on('change', (color, source, instance) => {
+      document.documentElement.style.setProperty("--primary-color", `${color.toHEXA().toString()}`);
+    })
 
+  }
 }
-
 export { animateNavbar }
